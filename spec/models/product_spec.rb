3e915f1d33590099ba8b18e9f124d934549eac3b
SHAJED <shajed_msh@yahoo.com>
2016-02-08 17:24:13 +0600

    Finishes modeling the product model along with user associations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,12 +13,5 @@ describe Product do
   it { should validate_presence_of :price }
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
   it { should validate_presence_of :user_id }
+  it { should belong_to :user }
 end
-
-=begin
-require 'rails_helper'
-
-RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-=end
